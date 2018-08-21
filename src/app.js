9fef00dcf5f2c01a44879d5eea44f69868259fd1
Jon Olson <jonolson64@outlook.com>
2018-08-21 17:49:48 -0400

    setup production webpack build

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import 'react-dates/initialize';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
 import configureStore from './store/configureStore';
 import AppRouter, { history } from './routers/AppRouter';
 import { firebase } from './firebase/firebase';
 import { startSetComics } from './actions/comics';
 import { login, logout } from './actions/auth';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
 const store = configureStore();
 
