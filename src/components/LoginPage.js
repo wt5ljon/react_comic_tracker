d230341efbc3e11eaf93b3b3137ef5f944476825
Jon Olson <jonolson64@outlook.com>
2018-08-24 21:23:10 -0400

    add styling to Login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,8 +4,12 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 const LoginPage = ({beginLogin}) => (
-  <div>
-    <button type="button" onClick={beginLogin}>Login</button>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">Comicbook Tracking App</h1>
+      <p>Keeping track of important comics!</p>
+      <button type="button" onClick={beginLogin}>Login</button>
+    </div>
   </div>
 );
 
